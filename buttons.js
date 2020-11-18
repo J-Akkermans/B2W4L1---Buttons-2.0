c7ff853ad8eb87d083a7cb14d2ea397ad4f19b2b
J Akkermans <99044668@mydavinci.nl>
2020-11-18 14:43:26 +0100

    still sad lmfao.

---
+++ buttons.js
@@ -0,0 +1,35 @@
+const imgID = document.getElementById("img-1");
+const containerBackground = document.getElementById("container");
+
+const buttonOne = document.getElementById("1");
+const buttonTwo = document.getElementById("2");
+const buttonThree = document.getElementById("3");
+
+var buttonOneCount = 1;
+var buttonTwoCount = 1;
+var buttonThreeCount = 1;
+
+function imgChange(id){
+    switch(id) {
+        case 1:
+          buttonOne.innerHTML = buttonOneCount++
+          imgID.src = "images/1.jpg";
+          containerBackground.style.backgroundImage="url(images/bg1.jpg)";
+          break;
+        case 2:
+          buttonTwo.innerHTML = buttonTwoCount++
+          imgID.src = "images/2.jpg";
+          containerBackground.style.backgroundImage="url(images/bg2.jpg)";
+          break;
+        case 3:
+          buttonThree.innerHTML = buttonThreeCount++
+          imgID.src = "images/3.jpg";
+          containerBackground.style.backgroundImage="url(images/bg3.jpg)";
+            break;
+        default:
+
+        break;
+      }
+}
+
+
